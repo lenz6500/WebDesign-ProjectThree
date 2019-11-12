d6aa14c1a32d619d481d34857880b4d87362ca35
Nick Hillemeier <hill6908@stthomas.edu>
2019-11-11 21:23:13 -0600

    Updated server.js, added database
    Implemented /codes, /neighborhoods, and almost implemented /incidents

--- server.js
+++ server.js
@@ -3,45 +3,77 @@ var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
 var json2xml = require('json2xml');
+var sqlite3 = require('sqlite3');
 
 var port = 8000;
-var public_dir = path.join(__dirname, 'public');
+var db_filename = path.join(__dirname, 'db', 'stpaul_crime.sqlite3');
 
 var app = express();
-app.use(express.static(public_dir));
-app.use(bodyParser.urlencoded({extended: true})); //use in .put
+app.use(bodyParser.urlencoded({extended: true}));
 
-var users;
+var codesObject = new Object;
+var neighborhoodsObject = new Object;
+var incidentsObject = new Object;
 
-fs.readFile(path.join(public_dir, 'list.json'), (err, data) => {
-	if(err){
-		console.log("Error: could not read users.json");
-		users = {users: []};
-	} else {
-		users = JSON.parse(data);
+var db = new sqlite3.Database(db_filename, sqlite3.OPEN_READONLY, (err) => {
+    if (err) {
+        console.log('Error opening ' + db_filename);
+    }
+    else {
+        console.log('Now connected to ' + db_filename);
+    }
+});
+
+db.all("SELECT * FROM Codes ORDER BY code", (err, rows) => {
+	for(var i = 0; i < rows.length; i++){
+		codesObject[rows[i].code] = rows[i].incident_type;
 	}
 });
 
-app.get('/list-users', (req, res) => {	
-	var users2 = JSON.parse(JSON.stringify(users));
-	if(req.query.limit != undefined){
-		users2.users.splice(req.query.limit, users2.users.length);
-	}
-	if(req.query.format == 'xml'){
-		users2 = json2xml(users2);
-		res.type('xml').send(users2);
-	}else{
-		res.type('json').send(users2);
+db.all("SELECT * FROM Neighborhoods ORDER BY neighborhood_number", (err, rows) => {
+	for(var i = 0; i < rows.length; i++){
+		neighborhoodsObject[rows[i].neighborhood_number] = rows[i].neighborhood_name;
 	}
 });
 
-app.put('/add-user', (req, res) => {
+/* Need to fix this */
+db.all("SELECT * FROM Incidents ORDER BY date_time DESC", (err, rows) => {
+	for(var i = 0; i < rows.length; i++){
+		var newDate = rows[i].date_time.substring(0,10);
+		var newTime = rows[i].date_time.substring(11);
+		var report = {
+			date: newDate,
+			time: newTime,
+			code: rows[i].code,
+			incident: rows[i].incident,
+			police_grid: rows[i].police_grid,
+			neighborhood_number: rows[i].neighborhood_number,
+			block: rows[i].block
+		};
+		incidentsObject[rows[i].case_number] = report;
+	}
+});
+
+
+app.get('/codes', (req, res) => {
+	res.type('json').send(JSON.stringify(codesObject, null, 4));
+});
+
+app.get('/neighborhoods', (req, res) => {
+	res.type('json').send(JSON.stringify(neighborhoodsObject, null, 4));
+});
+
+app.get('/incidents', (req, res) => {
+	res.type('json').send(JSON.stringify(incidentsObject, null, 4));
+});
+
+app.put('/new_incident', (req, res) => {
 	var newUser = {
 		id: parseInt(req.body.id, 10),
 		name: req.body.name,
 		email: req.body.email
 	};
-	
+
 	var hasBeenUsed = false;
 	for(let i = 0; i < users.users.length; i++){
 		if(users.users[i].id == newUser.id){
@@ -59,47 +91,6 @@ app.put('/add-user', (req, res) => {
 	}
 });
 
-app.delete('/remove-user', (req, res) => {
-	for(let i = 0; i < users.users.length; i++){
-		if(users.users[i].id == parseInt(req.body.id, 10)){
-			users.users.splice(i, 1);
-			fs.writeFile(path.join(public_dir, 'list.json'), JSON.stringify(users, null, 4), (err) => {
-				res.status(200).send('Successfully removed the user.');
-			});
-		} else {
-				console.log("ERROR: Attempt to remove user failed because User ID did not exist.");
-				res.status(500).send('Error: User ID was not found in database.');
-		}
-	}
-});
-
-app.post('/update-user', (req, res) => {	
-	var updatedUser = {
-		id: parseInt(req.body.id, 10),
-		name: req.body.name,
-		email: req.body.email
-	};
-	var userExists = false;
-	for(let i = 0; i < users.users.length; i++){
-		if(users.users[i].id == updatedUser.id){
-			userExists = true;
-		}
-	}
-	if(!userExists) {
-		console.log("ERROR: Attempt to update user failed because User ID did not exist.");
-		res.status(500).send('Error: User ID not in database.');
-	} else {
-		for(let i = 0; i < users.users.length; i++){
-			if(users.users[i].id == parseInt(req.body.id, 10)){
-				users.users[i].name = req.body.name;
-				users.users[i].email = req.body.email;
-				fs.writeFile(path.join(public_dir, 'list.json'), JSON.stringify(users, null, 4), (err) => {
-					res.status(200).send('Successfully updated the user.');
-				});
-			}
-		}
-	}
-});
 
 function ReadFile(filename) { //Simple read file option.
     return new Promise((resolve, reject) => {
